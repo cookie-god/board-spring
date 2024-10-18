ad95a2b8c63652da2cdd3234c9fbf783b51843c4
cookie-god <lion0193@gmail.com>
2024-10-18 18:49:29 +0900

    feat: 스웨거 문서 토큰 추가 완료

--- src/main/java/kisung/template/board/config/SwaggerConfig.java
+++ src/main/java/kisung/template/board/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package kisung.template.board.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,14 +13,21 @@ public class SwaggerConfig {
   @Bean
   public OpenAPI openAPI() {
     return new OpenAPI()
-        .components(new Components())
-        .info(apiInfo());
+      .components(new Components()
+        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+          .type(SecurityScheme.Type.HTTP)
+          .scheme("bearer")
+          .bearerFormat("JWT")
+        )
+      )
+      .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+      .info(apiInfo());
   }
 
   private Info apiInfo() {
     return new Info()
-        .title("board swagger")
-        .description("게시판 API 입니다.")
-        .version("1.0.0");
+      .title("board swagger")
+      .description("게시판 API 입니다.")
+      .version("1.0.0");
   }
 }
