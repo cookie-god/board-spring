91ba4140f8576c70ccd5b51fb29fbba9e3b25390
lion0193@gmail.com <lion0193@gmail.com>
2024-10-28 20:55:53 +0900

    check

---
+++ src/main/generated/kisung/template/board/entity/QUserInfo.java
@@ -0,0 +1,56 @@
+package kisung.template.board.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUserInfo is a Querydsl query type for UserInfo
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUserInfo extends EntityPathBase<UserInfo> {
+
+    private static final long serialVersionUID = 1693149693L;
+
+    public static final QUserInfo userInfo = new QUserInfo("userInfo");
+
+    public final kisung.template.board.entity.base.QBaseEntity _super = new kisung.template.board.entity.base.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    public final StringPath role = createString("role");
+
+    //inherited
+    public final StringPath status = _super.status;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QUserInfo(String variable) {
+        super(UserInfo.class, forVariable(variable));
+    }
+
+    public QUserInfo(Path<? extends UserInfo> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUserInfo(PathMetadata metadata) {
+        super(UserInfo.class, metadata);
+    }
+
+}
+
