2e9fe3746e41168635d28b387dd73adcb2b81fb8
cookie-god <lion0193@gmail.com>
2024-11-04 16:28:18 +0900

    feat: 빈 목록 조회 테스트

--- src/test/java/kisung/template/board/BoardApplicationTests.java
+++ src/test/java/kisung/template/board/BoardApplicationTests.java
@@ -1,13 +1,22 @@
 package kisung.template.board;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BoardApplicationTests {
-
+	@Autowired
+	private ApplicationContext applicationContext;
 	@Test
 	void contextLoads() {
+		if (applicationContext != null) {
+			String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+			for (String bean: beanDefinitionNames) {
+				System.out.println("bean = " + bean);
+			}
+		}
 	}
 
 }
