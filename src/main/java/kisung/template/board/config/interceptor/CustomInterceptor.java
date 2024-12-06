1386d3f8161ab8266f660f0d7d856bc5661724e5
cookie-god <lion0193@gmail.com>
2024-12-06 18:57:56 +0900

    feat: 인터셉터 체크

--- src/main/java/kisung/template/board/config/interceptor/CustomInterceptor.java
+++ src/main/java/kisung/template/board/config/interceptor/CustomInterceptor.java
@@ -14,11 +14,13 @@ public class CustomInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     log.info("start interceptor");
+    log.info(request.getContextPath());
     return true;
   }
 
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
     log.info("end interceptor");
+    log.info(response.getContentType());
   }
 }
