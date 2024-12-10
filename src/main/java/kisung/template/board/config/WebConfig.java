27dd555c10fe7f0c2941f0b7dfe939a95f790c03
cookie-god <lion0193@gmail.com>
2024-12-10 18:08:18 +0900

    setting: cors 설정 완료

--- src/main/java/kisung/template/board/config/WebConfig.java
+++ src/main/java/kisung/template/board/config/WebConfig.java
@@ -3,6 +3,7 @@ package kisung.template.board.config;
 import kisung.template.board.config.interceptor.CustomInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(customInterceptor)
         .addPathPatterns("/**");
+  }
+
+  @Override
+
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins("*")
+      .allowedMethods("*")
+      .allowCredentials(true);
   }
 }
