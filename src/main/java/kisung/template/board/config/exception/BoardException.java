6960413ace47c2a5820e68277cf81d08abbd2df1
cookie-god <lion0193@gmail.com>
2024-12-15 22:00:44 +0900

    fix: final 처리

--- src/main/java/kisung/template/board/config/exception/BoardException.java
+++ src/main/java/kisung/template/board/config/exception/BoardException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 public class BoardException extends RuntimeException {
-  private ErrorCode errorCode;
+  private final ErrorCode errorCode;
 
   public BoardException(ErrorCode errorCode) {
     super(errorCode.getMessage());
