bf37dd0541f5c024cbbe059a73b4e9eaa6d00c32
cookie-god <lion0193@gmail.com>
2024-12-16 22:27:58 +0900

    feat: 개행 처리

--- src/main/java/kisung/template/board/config/QueryDslConfig.java
+++ src/main/java/kisung/template/board/config/QueryDslConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class QueryDslConfig {
   @PersistenceContext
   private EntityManager entityManager;
-
   @Bean
   public JPAQueryFactory jpaQueryFactory() {
     return new JPAQueryFactory(entityManager);
